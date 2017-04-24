6e163418d0c4738a691b494a4d146dea5d0350db
Yoonje Choi <yoonjechoi@gmail.com>
2017-04-24 11:06:08 +0900

    test(pagenation): add test for pagination when the entries in the front are removed
    https://app.asana.com/0/308727253154577/325615362685426

--- selstagram2/instagram/tests.py
+++ selstagram2/instagram/tests.py
@@ -125,6 +125,32 @@ class MediaAPITest(test_mixins.InstagramMediaMixin, APITestCase):
         self.assertEqual(len(ids), default_limit)
         self.assertSequenceEqual(ids, list(map(lambda item: item['id'], media_.results)))
 
+    def test_media_pagenation_with_removal_front_entries(self):
+        # Given : Create 1000 dummy InstagramMedia and delete 250 media from id=1 to id=250
+        size = 1000
+        self.create_instagram_media(size)
+        number_of_object_to_delete = 250
+        delete_result = instagram_models.InstagramMedia.objects.\
+            filter(id__lte=number_of_object_to_delete).delete()
+
+        self.assertEqual(delete_result[0], number_of_object_to_delete)
+
+        # When : Invoking media api with offset=37
+        offset = 0
+        response = self.client.get('/tags/셀스타그램/media/'
+                                   '?offset={offset}'.format(offset=offset))
+
+        # Then : The numbers of media received is as same as
+        # InstagramMediaPageNation.default_limit
+        default_limit = LimitOffsetPagination.default_limit
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        media_ = Munch(response.data)
+        self.assertEqual(len(media_.results), default_limit)
+
+        ids = range(offset + 1 + number_of_object_to_delete, offset + default_limit + 1 + number_of_object_to_delete)
+        self.assertEqual(len(ids), default_limit)
+        self.assertSequenceEqual(ids, list(map(lambda item: item['id'], media_.results)))
+
 
 class CrawlingTaskTest(TestCase):
     def test_instagram_crawler(self):
